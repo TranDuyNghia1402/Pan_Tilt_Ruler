d43852d8619effa5570de25108dc164db776cb85
nghiatd <tranduynghia2002@gmail.com>
2024-09-14 15:09:07 +0700

    init class panruler

--- ruler/panruler.h
+++ ruler/panruler.h
@@ -7,6 +7,12 @@ class PanRuler
 {
 public:
     PanRuler(LvBaseObject *parent, const int32_t &width = 60, const int32_t &height = 600);
+
+private:
+    int32_t mWidth = 60;
+    int32_t mHeight = 600;
+
+    int32_t mCurrentScalePos = 0;
 };
 
 #endif // PANRULER_H
